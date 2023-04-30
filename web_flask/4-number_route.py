b7a8b5d324c531d776151a75b4dc43eb3974764d
elvis wangari <elviswangare@gmail.com>
2023-05-01 00:02:12 +0300

    is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """returns Hello Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """new rout to display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """displays `C` followed by value from the variable text"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """display python followed by text from variable or default   is cool"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    """returns a number"""
+    return "{:d} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
