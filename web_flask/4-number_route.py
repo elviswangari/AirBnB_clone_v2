b4d6b9eefb4739cc8ce8bbe81eadb601353a62b6
elvis wangari <elviswangare@gmail.com>
2023-05-01 00:03:45 +0300

    is a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,7 +32,7 @@ def python(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """returns a number"""
     return "{:d} is a number".format(n)
